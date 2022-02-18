1c77f1fb2abbb12d8a622cb6c0b41dfdf1999dee
bargkcan <73115798+bargkcan@users.noreply.github.com>
2022-02-18 13:11:56 +0300

    Update Man_in_the_middle_attack.py

--- Man_in_the_middle_attack.py
+++ Man_in_the_middle_attack.py
@@ -4,9 +4,7 @@ import optparse
 
 def get_mac_address(ip):
     arp_request_packet = scapy.ARP(pdst=ip)
-    #scapy.ls(scapy.ARP())
     broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    #scapy.ls(scapy.Ether())
     combined_packet = broadcast_packet/arp_request_packet
     answered_list = scapy.srp(combined_packet,timeout=1,verbose=False)[0]
 
